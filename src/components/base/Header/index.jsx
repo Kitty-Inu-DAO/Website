661d64030746c5fbdad1c46c5ab118a87172e98a
Rygel <rygel@rygel.rygel>
2023-06-08 16:07:50 +0200

    improvement: more readability improvements

--- src/components/base/Header/index.jsx
+++ src/components/base/Header/index.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='px-20 py-10 flex justify-end items-center bar-styling' style={{ backdropFilter: 'blur(20px)', color: '#fff', backgroundColor: 'transparent', boxShadow: '0px 10px 20px rgba(0,0,0,0.19), 0px 6px 6px rgba(0,0,0,0.23)' }}>
       <div className='flex justify-start w-1/2'>
-        <h2>
+        <h2 className="kitty-inu-dao">
           <span style={{ color: '#e43a95' }}>Kitty</span>
           <span style={{ color: '#50c5d4' }}> Inu </span>
           DAO
