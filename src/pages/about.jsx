2fd4572fe57a1f833241e0c4b733d7f07a623960
Rygel <rygel@rygel.rygel>
2023-06-10 14:24:00 +0200

    fix: direct loading of page caused a hydration error

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,110 +1,112 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
-import { motion, useAnimation } from 'framer-motion'
+import { useAnimation } from 'framer-motion'
 import ReactPlayer from 'react-player'
 
 import Header from '@/components/base/Header/';
 import Hero from '@/components/base/Hero'
 import HomeFooter from '@/components/base/Footer/Home'
 
-const teamVariant = {
-  visible: { opacity: 1, scale: 1, y: 0, transition: { duration: 0.5 } },
-  hidden: { opacity: 0, scale: 1, y: 60 },
-}
-
 export default function About() {
-  const control = useAnimation()
-  const [ref, inView] = useInView()
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+    const [hasWindow, setHasWindow] = useState(false);
 
-  useEffect(() => {
-    if (inView) {
-      control.start('visible')
-    } else {
-      control.start('hidden')
-    }
-  }, [control, inView])
+    useEffect(() => {
+        if (inView) {
+            control.start('visible').then();
+        } else {
+            control.start('hidden').then();
+        }
+    }, [control, inView])
 
-  return (
-    <>
-      <Header />
-      <main className='m-auto w-11/12 bg-sky-800/30 backdrop-blur-md lg:w-10/12 xl:w-8/12 pb-10'>
-        <section className='px-5 sm:px-10 pt-4 pb-16 rounded-b-lg'>
-          <Hero title='About' description='' />
-          <div className='flex'>
-            <div className='flex-1 video-wrapper mt-4'>
-              <div>
-                <ReactPlayer
-                  playing={false}
-                  url='/img/about/kittys_back.webm'
-                  width='100%'
-                  height='100%'
-                  controls={true}
-                />
-              </div>
-            </div>
-          </div>
-          <h2 className='mt-20 mb-5 text-center text-shadow'>This ain't your grandma's knitting circle</h2>
-          <p className='text-white text-center mt-8'>
-            We're a decentralized pack of meme coin junkies, gaming gurus, creative technomancers, and art aficionados. Here, we're about more than just raising the roof; we're about raising the woof...err... meow?
-          </p>
-          <h2 className='mt-16 mb-5 text-center text-shadow'>We Govern Stuff...</h2>
-          <p className='text-white text-center mt-8'>
-            Fueling our meme-powered rocket is the Kitty Inu token, the ERC20 certified digital tuna that governs our DAO. It's not just another fish in the sea; it's your boarding pass into the fur-midable world of DAO governance. Remember, in Kitty Inu DAO, your vote doesn't just count; it purrs!
-          </p>
-          <h2 className='mt-16 mb-5 text-shadow text-center'>cat-doge of the interwebz</h2>
-          <div className='flex gap-10 mb-8 mt-8 items-center'>
-            <div className='flex-1 justify-center'>
-              <p className='text-white'>
-                The star of our meme-verse is the one and only Kitty Inu, licensed by 9VRSE Inc. As the reigning cat-doge of the interwebz, Kitty Inu is the embodiment of all that's cool and claw-some in the DAO world. And no, Kitty Inu is not up for adoption; we're all just its humble servants in this decentralized playground.
-              </p>
-            </div>
-            <div className='flex-1 mt-8'>
-              <div>
-                <ReactPlayer
-                  playing={false}
-                  url='/img/about/kitty_news.webm'
-                  width='100%'
-                  height='100%'
-                  controls={true}
-                />
-              </div>
-            </div>
-          </div>
-          <h2 className='mt-16 mb-5 text-shadow text-center'>Stacks on Deck</h2>
-          <p className='text-white mt-8'>
-            But hey, we're not just here to play; we're here to PAW-er up the community. Our DAO's treasury is the beating heart that pumps liquid gold - or should we say milk? - into community initiatives. We're all about that diamond paws life, hodling together and propelling projects that give everyone in the DAO a reason to say "GM!"
-          </p>
-          <h2 className='mt-16 mb-5 text-shadow text-center'>Kitty next shib</h2>
-          <p className='text-white text-center'>
-            So, are you tired of all those grumpy cat memes telling you to "go away?" Are you ready to dive into a vibrant, engaging, and downright quirky community where the phrase "Cats rule the Internet" is taken a tad too literally? Well, then Kitty Inu DAO is the purr-fect place for you!
-          </p>
-          <p className='mt-16 text-center text-shadow mb-16'>
-            Get ready to put those laser eyes to work and join us in making every day Caturday. To infinity and beyond, frens!
-          </p>
-          <div class='flex'>
-            <div className='flex-1 video-wrapper'>
-              <div>
-                <ReactPlayer
-                  playing={false}
-                  url='/img/about/kitty_dance.webm'
-                  width='100%'
-                  height='100%'
-                  controls={true}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <HomeFooter />
-    </>
-  )
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setHasWindow(true);
+        }
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <main className='m-auto w-11/12 bg-sky-800/30 backdrop-blur-md lg:w-10/12 xl:w-8/12 pb-10'>
+                <section className='px-5 sm:px-10 pt-4 pb-16 rounded-b-lg'>
+                    <Hero title='About' description='' />
+                    <div className='flex'>
+                        <div className='flex-1 video-wrapper mt-4'>
+                            <div>
+                                {hasWindow && <ReactPlayer
+                                    playing={false}
+                                    url='/img/about/kittys_back.webm'
+                                    width='100%'
+                                    height='100%'
+                                    controls={true}
+                                />}
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className='mt-20 mb-5 text-center text-shadow'>This ain't your grandma's knitting circle</h2>
+                    <p className='text-white text-center mt-8'>
+                        We're a decentralized pack of meme coin junkies, gaming gurus, creative technomancers, and art aficionados. Here, we're about more than just raising the roof; we're about raising the woof...err... meow?
+                    </p>
+                    <h2 className='mt-16 mb-5 text-center text-shadow'>We Govern Stuff...</h2>
+                    <p className='text-white text-center mt-8'>
+                        Fueling our meme-powered rocket is the Kitty Inu token, the ERC20 certified digital tuna that governs our DAO. It's not just another fish in the sea; it's your boarding pass into the fur-midable world of DAO governance. Remember, in Kitty Inu DAO, your vote doesn't just count; it purrs!
+                    </p>
+                    <h2 className='mt-16 mb-5 text-shadow text-center'>cat-doge of the interwebz</h2>
+                    <div className='flex gap-10 mb-8 mt-8 items-center'>
+                        <div className='flex-1 justify-center'>
+                            <p className='text-white'>
+                                The star of our meme-verse is the one and only Kitty Inu, licensed by 9VRSE Inc. As the reigning cat-doge of the interwebz, Kitty Inu is the embodiment of all that's cool and claw-some in the DAO world. And no, Kitty Inu is not up for adoption; we're all just its humble servants in this decentralized playground.
+                            </p>
+                        </div>
+                        <div className='flex-1 mt-8'>
+                            <div>
+                                {hasWindow && <ReactPlayer
+                                    playing={false}
+                                    url='/img/about/kitty_news.webm'
+                                    width='100%'
+                                    height='100%'
+                                    controls={true}
+                                />}
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className='mt-16 mb-5 text-shadow text-center'>Stacks on Deck</h2>
+                    <p className='text-white mt-8'>
+                        But hey, we're not just here to play; we're here to PAW-er up the community. Our DAO's treasury is the beating heart that pumps liquid gold - or should we say milk? - into community initiatives. We're all about that diamond paws life, hodling together and propelling projects that give everyone in the DAO a reason to say "GM!"
+                    </p>
+                    <h2 className='mt-16 mb-5 text-shadow text-center'>Kitty next shib</h2>
+                    <p className='text-white text-center'>
+                        So, are you tired of all those grumpy cat memes telling you to "go away?" Are you ready to dive into a vibrant, engaging, and downright quirky community where the phrase "Cats rule the Internet" is taken a tad too literally? Well, then Kitty Inu DAO is the purr-fect place for you!
+                    </p>
+                    <p className='mt-16 text-center text-shadow mb-16'>
+                        Get ready to put those laser eyes to work and join us in making every day Caturday. To infinity and beyond, frens!
+                    </p>
+                    <div className='flex'>
+                        <div className='flex-1 video-wrapper'>
+                            <div>
+                                {hasWindow && <ReactPlayer
+                                    playing={false}
+                                    url='/img/about/kitty_dance.webm'
+                                    width='100%'
+                                    height='100%'
+                                    controls={true}
+                                />}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <HomeFooter />
+        </>
+    )
 }
 
 export async function getStaticProps() {
-  return {
-    props: {
-      title: 'About',
-    },
-  }
+    return {
+        props: {
+            title: 'About',
+        },
+    }
 }
