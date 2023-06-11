4f96e1a2f0daac4e8c57b672e8e00377da33be3e
Rygel <rygel@rygel.rygel>
2023-06-11 19:15:50 +0200

    improvement: added the roadmap to the site menu

--- src/pages/roadmap.jsx
+++ src/pages/roadmap.jsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import {useState} from "react";
-import Link from "next/link";
 
 import {roadmap} from "@/components/roadmap/roadmap";
 
