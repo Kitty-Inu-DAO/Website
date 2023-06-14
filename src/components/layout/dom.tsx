1d8d5691e36083c66aa8a1132484fdb343501012
Rygel <rygel@rygel.rygel>
2023-06-14 16:59:41 +0200

    refactoring: only use absolute positioning for home

--- src/components/layout/dom.tsx
+++ src/components/layout/dom.tsx
@@ -1,13 +1,16 @@
 import { setState } from '@/helpers/store'
 import { useEffect, useRef } from 'react'
 
-const Dom = ({ classes, children }) => {
+const Dom = ({ absolute, classes, children }) => {
   const ref = useRef(null)
   useEffect(() => {
     setState({ dom: ref })
   }, [])
 
-  const className = classes + " absolute top-0 left-0 z-10 w-screen h-full overflow-x-hidden dom"
+  let className = classes + " dom";
+  if (absolute) {
+    className += ' absolute top-0 left-0 z-10 w-screen h-full overflow-x-hidden dom';
+  }
   if (children) {
     console.log(children.props)
   }
