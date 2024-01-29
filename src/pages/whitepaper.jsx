143d9de06c2f042cfefbb588c5ba6dce651fe8dc
Rygel <rygel@rygel.rygel>
2024-01-29 15:51:47 +0100

    Added more explanations before the video

--- src/pages/whitepaper.jsx
+++ src/pages/whitepaper.jsx
@@ -7,6 +7,7 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import Header from '@/components/base/Header'
 import Hero from '@/components/base/Hero'
 import HomeFooter from '@/components/base/Footer/Home'
+import Link from "next/link";
 
 
 export default function Whitepaper() {
@@ -31,9 +32,22 @@ export default function Whitepaper() {
     return (
         <>
             <Header />
-            <Hero title='Whitepaper' description='' />
-            <main className='flex mt-8 flex-col items-center justify-center mx-auto mb-auto'>
+
+            <main className='flex flex-col items-center justify-center mx-auto mb-auto'>
+                <section className='w-11/12 lg:w-10/12 xl:w-8/12 flex flex-col items-center justify-center bg-[#b517b3]/30 px-10 pt-4 pb-28 rounded-b-lg backdrop-blur-md'>
+
+                <Hero title='Whitepaper' description='' />
+
                 <div className="w-full px-5 md:px-10 max-w-6xl mb-16">
+                    <p className="mt-4">
+                        The Kitty Inu DAO doesn't provide a traditional whitepaper.<br />
+                        We are a decentralized organisation that is solely guided by the decisions of our holders.<br />
+                        <span className="font-bold">Our whitepaper is represented by our past work, what we're currently building and what the $kitty holders will decide to do in the future.</span>
+                    </p>
+                    <p className="mt-4 mb-16">
+                        Please check our <Link href="/roadmap" className="underline hover:no-underline">Roadmap</Link> and <Link href="https://docs-kittyinudao.eth.limo/" className="underline hover:no-underline">Governance</Link> pages for more details.
+                        And let's build together!
+                    </p>
                     {!videoStarted ?
                         <>
                             <div className='flex items-center justify-center text-center'>
@@ -58,6 +72,8 @@ export default function Whitepaper() {
                         </>
                     }
                 </div>
+
+                </section>
             </main>
             <HomeFooter />
         </>
